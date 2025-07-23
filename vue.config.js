867569ebd893e7ba7f8914558c9a8cd2d92aee3d
Jes360 <s222598312@deakin.edu.au>
2025-07-23 18:48:50 +1000

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  
+  // THIS IS THE CRITICAL FIX
+  publicPath: '/vue-art-store/'
 })
